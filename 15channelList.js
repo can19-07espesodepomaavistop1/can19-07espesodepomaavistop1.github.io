17932ab5c684023be3d719bd76297f228e9316c9
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-07-19 02:45:20 -0300

    Create 15channelList.js

---
+++ 15channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'foo 1 arge',
+    getURL: 'Rm94U3BvcnRz',
+    keyId: "cbc2a0c7a38df0aa3333cd71841d3b0d",
+    key: "aac61b730e2ac1df23f1e872e7541c1b",
+    number: 3,
+   
+  }
+]
