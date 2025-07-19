29e7753933c5ca6cf40c63d7b8819b8b753f918c
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-07-19 02:31:49 -0300

    Create 12channelList.js

---
+++ 12channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'espee pre arge',
+    getURL: 'VE5UX1Nwb3J0c19IRA',
+    keyId: "bc80fa3da696e503f940fde5f681a1c5",
+    key: "ea46e4e9f1132e8dd71fb77f7d55058a",
+    number: 6,
+   
+  }
+]
