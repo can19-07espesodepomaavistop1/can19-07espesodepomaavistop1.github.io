4cc63e7748a9349fc3e75ef7fef5599b7e724ada
can13-05-2025maavistop3 <candepomasvistosop3@protonmail.com>
2025-07-19 00:04:18 -0300

    Update 2channelList.js

--- 2channelList.js
+++ 2channelList.js
@@ -1,1 +1,13 @@
+ const channelList = [
 
+
+  {
+  name: 'FLOO arge 2',
+    getURL: 'RXZlbnRvc18yX0hE',
+    keyId: "e54e9ea87a634658fbba0e380aa701a7",
+    key: "4e486d25d7d0e7d131743b285963c643",
+    number: 3,
+   
+  }
+
+]
