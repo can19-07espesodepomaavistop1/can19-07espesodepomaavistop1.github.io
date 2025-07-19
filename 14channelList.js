5eec7042f3e8830439b94946884d4b740101e022
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-07-19 02:41:10 -0300

    Create 14channelList.js

---
+++ 14channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'teincee inteeer',
+    getURL: 'VHlDX0ludGVybmFjaW9uYWw',
+    keyId: "58e1ebe75d944f6a98ea67b1c7c0a572",
+    key: "1c311b069dbce31ce8e62a6e7e2433eb",
+    number: 3,
+   
+  }
+]
