4380c2c24db64a6deaa7a4e8caf3651072915ab9
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-07-19 03:07:18 -0300

    Update 9channelList.js

--- 9channelList.js
+++ 9channelList.js
@@ -1,7 +1,7 @@
  const channelList = [
   {
-  name: 'espee arge 6',
-    getURL: 'Rm94U3BvcnRzM19VWQ==',
+  name: 'espee arge 7',
+    getURL: 'Rm94U3BvcnRzMl9VWQ==',
     keyId: "5fc7b662e69c0fbf8d39691166b1c919",
     key: "89157cdd25df25d56f1ecccee3850888",
     number: 7,
