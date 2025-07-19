9104c882d586eda73269ec4cba455f13b7a771ef
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-07-19 02:25:33 -0300

    Create 10channelList.js

---
+++ 10channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'espee arge 7',
+    getURL: 'Rm94U3BvcnRzMl9VWQ==',
+    keyId: "5fc7b662e69c0fbf8d39691166b1c919",
+    key: "89157cdd25df25d56f1ecccee3850888",
+    number: 7,
+   
+  }
+]
