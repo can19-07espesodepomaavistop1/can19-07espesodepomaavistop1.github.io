1ac9441a9b8d414f476fdf6c6e4c2f270eedcbaa
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-07-19 03:17:36 -0300

    Update 12channelList.js

--- 12channelList.js
+++ 12channelList.js
@@ -1,10 +1,10 @@
  const channelList = [
   {
-  name: 'teincee arge',
-    getURL: 'VHlDU3BvcnQ',
-    keyId: "2b21c8fa9a329cce311a4c4a4aa996a1",
-    key: "cc23ea1fb32629f9e1f48c8deeae3e5b",
-    number: 7,
+  name: 'teincee inteeer',
+    getURL: 'VHlDX0ludGVybmFjaW9uYWw',
+    keyId: "58e1ebe75d944f6a98ea67b1c7c0a572",
+    key: "1c311b069dbce31ce8e62a6e7e2433eb",
+    number: 3,
    
   }
 ]
