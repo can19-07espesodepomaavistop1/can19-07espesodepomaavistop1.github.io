a1717a8bb5071917407a94f7c953277298cf1d2f
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-07-19 02:37:45 -0300

    Create 13channelList.js

---
+++ 13channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'teincee arge',
+    getURL: 'VHlDU3BvcnQ',
+    keyId: "2b21c8fa9a329cce311a4c4a4aa996a1",
+    key: "cc23ea1fb32629f9e1f48c8deeae3e5b",
+    number: 7,
+   
+  }
+]
