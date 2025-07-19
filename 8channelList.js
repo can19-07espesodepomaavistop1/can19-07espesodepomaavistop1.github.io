05b1ad84d17d507c1b878b48ad50b9716fc75321
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-07-19 02:16:57 -0300

    Create 8channelList.js

---
+++ 8channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'espee arge 5',
+    getURL: 'RVNQTjQ=',
+    keyId: "24f2b3e741f0d9e9a8d516faff38bddc",
+    key: "bbd3fd02fb104e1463ac528a13f67e4a",
+    number: 7,
+   
+  }
+]
