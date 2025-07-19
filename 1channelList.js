96763ec2e42fa08af4f593c76bdbde0523f245a2
can13-05-2025maavistop3 <candepomasvistosop3@protonmail.com>
2025-07-18 23:57:07 -0300

    Create 1channelList.js

---
+++ 1channelList.js
@@ -0,0 +1,13 @@
+ const channelList = [
+
+
+  {
+  name: 'FLOO arge 1',
+    getURL: 'RXZlbnRvczFIRA',
+    keyId: "5c86e237317c2231ac1bc130e995fad6",
+    key: "dcacf7b239ee03df3c69ca11dbcde4eb",
+    number: 3,
+   
+  }
+
+]
