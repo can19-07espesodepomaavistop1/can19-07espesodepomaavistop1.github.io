2da959dc0b1ea466e7ac497a7c2a1463a9dc7f67
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-07-26 01:35:38 -0300

    Update 17channelList.js

--- 17channelList.js
+++ 17channelList.js
@@ -1,1 +1,10 @@
-
+ const channelList = [
+  {
+  name: 'foo 1 uyyy',
+    getURL: 'RXZlbnRvc19IRF9VeQ==',
+    keyId: "48d0e34c8797c5c2a742d2630a8fb975",
+    key: "fb5d12b9d8febe836e5670abd003ddca",
+    number: 7,
+   
+  }
+]
