62753f43ffcdc842d8f78ebd56c33c3346e593e6
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-07-28 03:24:13 -0300

    Create 20channelList.js

---
+++ 20channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'foo muu 607',
+    getURL: 'Rmxvd19NdXNpY18z',
+    keyId:  "e078b15ed770ec71f803c0ecc43de033",
+    key: "7010bccda544f74d1b425c4cebd082d4",
+    number: 7,
+   
+  }
+]
