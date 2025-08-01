d505c78df3f2b4508d6cb6310a33e4238223575b
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-08-01 01:30:21 -0300

    Create 23channelList.js

---
+++ 23channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'emmteevehii ',
+    getURL: 'TVRWX0hpdHM=',
+    keyId: "61008dfc867544cd872de99b1f2b82cf",
+    key:  "716449756316b91c54803aaa22a2fbf0",
+    number: 6,
+   
+  }
+]
