650e365d5755ed1f166379d491641555b441a5c7
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-08-01 01:32:15 -0300

    Create 24channelList.js

---
+++ 24channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'emmteeve00 ',
+    getURL: 'TVRWMDA=',
+    keyId: "1f0c09ed9e5841cf867ba6eb3cdfd61d",
+    key:   "802c89c6bae6a245aaafcf40c1986fc1",
+    number: 7,
+   
+  }
+]
