67bdf5eaaca082503fefe9f3dfb9b02c7239b1fd
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-08-01 02:00:39 -0300

    Update 22channelList.js

--- 22channelList.js
+++ 22channelList.js
@@ -1,7 +1,7 @@
  const channelList = [
   {
   name: 'emmteeve ',
-    getURL: 'TVRWMDA=',
+    getURL: 'TVRWX0hE',
     keyId: "0c9eb3ead38a122ac460ad96a8ebfd2e",
     key: "66bfbfa4449eb8bc1bcf7577d5bffaad",
     number: 6,
