e6706c9c46cae7b0d320c81f51e107a852a768f8
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-08-02 04:05:37 -0300

    Create 25channelList.js

---
+++ 25channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'canmuusi argee ',
+    getURL: 'Q00=',
+    keyId: "682f36b5736f4560951ca14b80d29524",
+    key:  "3accb729067a39b3b8143f1b447b9d25",
+    number: 6,
+   
+  }
+]
