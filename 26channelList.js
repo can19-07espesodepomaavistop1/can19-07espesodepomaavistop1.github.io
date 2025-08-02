83b40fac1a9289223d7be821732b5bf1faeb493c
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-08-02 05:11:19 -0300

    Create 26channelList.js

---
+++ 26channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'canacheeteevee argee ',
+    getURL: 'SFRW',
+    keyId: "daecef5fe32f4ce083c6a0c692755d6a",
+    key: "d4227f24389a9ba77293214b93eb0d7d",
+    number: 6,
+   
+  }
+]
