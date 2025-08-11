999d5749b1e74d901fd150b803c0d9cdcbdad164
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-08-11 03:17:27 -0300

    Create 29channelList.js

---
+++ 29channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'can animaaplaa argee ',
+    getURL: 'QW5pbWFsUGxhbmV0',
+    keyId: "4146a8ecbb0540dc807c6389ee87e0bc",
+    key: "0c3cdc1b3e4617c57361265e9fa4c5bd",
+    number: 7,
+   
+  }
+]
