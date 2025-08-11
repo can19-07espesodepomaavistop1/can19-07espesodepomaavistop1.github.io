b2954e7e6f41a8fc45076dab603f096dfd5d19fb
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-08-11 03:00:23 -0300

    Create 28channelList.js

---
+++ 28channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'can quiero muss argee ',
+    getURL: 'TmF0R2VvSEQ=',
+    keyId: "4be0c61ceddc62294cc9e23b627af53d",
+    key: "852c1935d11c3c53d0a1ceff3bb3bf16",
+    number: 3,
+   
+  }
+]
