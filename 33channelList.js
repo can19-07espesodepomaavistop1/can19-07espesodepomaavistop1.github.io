a877eced79b88a8569bcaa1362a730a3e976fb3e
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-08-15 04:17:54 -0300

    Create 33channelList.js

---
+++ 33channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'can disco sciee argee ',
+    getURL: 'RGlzY292ZXJ5X1NjaWVuY2U=',
+    keyId:"46ba409fb04e40fb86b466d97e3a9588",
+    key:"f129fb9b5d213e20e2abe529b8cf2597",
+    number: 6,
+   
+  }
+]
