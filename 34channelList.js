9108e381161c9d313d9498478e8fd7797b3a54e6
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-08-15 04:20:10 -0300

    Create 34channelList.js

---
+++ 34channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'can disco teeele argee ',
+    getURL: 'VExD',
+    keyId:"58761c7b2819491eb3a0d765842c341a",
+    key:"9dabc48f88bd7f266734e57501bd6f47",
+    number: 6,
+   
+  }
+]
