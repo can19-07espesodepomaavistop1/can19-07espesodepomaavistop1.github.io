eae2ba868c07142231aac9cec5ac697f04bf4ea7
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-08-15 04:23:29 -0300

    Create 35channelList.js

---
+++ 35channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'can disco iiidee argee ',
+    getURL: 'SUQ=',
+    keyId: "0956caf2e2bd41f49fdcead7cc94fe24",
+    key:"640c49578073a911938617eb4e652d6c",
+    number: 7,
+   
+  }
+]
