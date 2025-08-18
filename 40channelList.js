707868d9e51e50bb0aad35f25b3941783d39c623
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-08-18 03:03:29 -0300

    Create 40channelList.js

---
+++ 40channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'can fiiartt argee ',
+    getURL: 'RmlsbV9BcnRz',
+    keyId:"91c540d350f34f5386f3f1c77f74001f",
+    key:"54ee72810551da0cba0c45a8df651962",
+    number: 6,
+   
+  }
+]
