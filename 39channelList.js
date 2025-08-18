1524a03caa6543a114cef2180097b0473f9542a3
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-08-18 03:00:51 -0300

    Create 39channelList.js

---
+++ 39channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'can hiistoo 2 argee ',
+    getURL: 'SGlzdG9yeV8y',
+    keyId:"ecbe97ec34784304b390a12ba3854ccf",
+    key:"5a0ee36ed9b8920cb19546fd92f2d415",
+    number: 6,
+   
+  }
+]
