089b8b020cf7391e4cb53c3e18e16553fd0574b8
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-08-18 03:05:16 -0300

    Create 41channelList.js

---
+++ 41channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'can suuchaa argee ',
+    getURL: 'U3VuX0NoYW5uZWw=',
+    keyId:"158177692aa3463eae5f4e1f72403e9c",
+    key:"1c6c026fba9cf7ecb576c7bedce0ead4",
+    number: 6,
+   
+  }
+]
