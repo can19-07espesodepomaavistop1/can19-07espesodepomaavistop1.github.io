1fea8397d40ee10c74b9ed72ab05237b8edf063b
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-09-05 22:49:50 -0300

    Create 45channelList.js

---
+++ 45channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'can peequii espoo argee ',
+    getURL: 'UFhfU3BvcnRz',
+    keyId:"9393321daef1463fc7e2e060298db852",
+    key:"be6bc91e17816f67e62596bed463c5ad",
+    number: 7,
+   
+  }
+]
