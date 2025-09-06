0366e67a3795cd4fa8b4cd8090aee7ff099c4e0c
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-09-05 22:40:45 -0300

    Update 42channelList.js

--- 42channelList.js
+++ 42channelList.js
@@ -3,7 +3,7 @@
   name: 'can dewechaespa argee ',
     getURL: 'RFc=',
     keyId:"e21fc12c6ee185fbc90a052b05026be9",
-    key:"5a0ee36ed9b8920cb19546fd92f2d415""0f96fd52903707aac0b42853a043ffd0",
+    key: "0f96fd52903707aac0b42853a043ffd0",
     number: 6,
    
   }
