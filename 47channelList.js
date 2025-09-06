375a4103d29a0ba249045ce81cc82755c194cf52
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-09-05 22:56:16 -0300

    Create 47channelList.js

---
+++ 47channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'can ameeeri espoo argee ',
+    getURL: 'QW1lcmljYV9TcG9ydHM=',
+    keyId:"a0aa01337a2148bda1e21862295ae037",
+    key:"f86f8457438d4e449d53b956cc1be664",
+    number: 6,
+   
+  }
+]
