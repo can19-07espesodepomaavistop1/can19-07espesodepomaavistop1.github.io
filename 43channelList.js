5d522c556c08a93e333e649093cdbd497243b2ca
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-09-05 22:42:14 -0300

    Create 43channelList.js

---
+++ 43channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'can veetee 1 uyy ',
+    getURL: 'VlRWX0hE',
+    keyId:"b96960aa715a4e51b7e952e81c99ac8e",
+    key:"5bbffc680764dbd63d90b774b3aa0c0a",
+    number: 3,
+   
+  }
+]
