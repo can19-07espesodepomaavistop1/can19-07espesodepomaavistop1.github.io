7bc4d7099846c20dc3fa863f6f971cb5f3670ca7
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-09-05 23:00:19 -0300

    Create 49channelList.js

---
+++ 49channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'can depoteeve argee ',
+    getURL: 'RGVwb3JUVkhE',
+    keyId:"f362097815892a4b83950f1d106ff0b2",
+    key:"4349e42e81c31a8389f4a42fcd298928",
+    number: 3,
+   
+  }
+]
