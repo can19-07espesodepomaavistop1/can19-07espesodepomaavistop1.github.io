159c2b45da5c037adafb35f2de294223ff5d4a4a
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-09-05 22:58:32 -0300

    Create 48channelList.js

---
+++ 48channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'can garagee tee argee ',
+    getURL: 'RWxfR2FyYWdl',
+    keyId:"03d1dfb8df034d8b81e57753df00a36d",
+    key:"08d83660db4a73bb1d61d60b09df5f4d",
+    number: 6,
+   
+  }
+]
