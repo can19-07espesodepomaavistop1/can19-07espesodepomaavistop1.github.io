d5f9242d6ab778807dc885bb923def2c3eb83f1b
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-09-05 23:02:45 -0300

    Create 50channelList.js

---
+++ 50channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'can neeeveeaa argee ',
+    getURL: 'TkJBX1RW',
+    keyId:"d0c38de3c9844e4e9f975dffb3eff8ad",
+    key:"141ca0fdf6ebadfa7107576b8e09e117",
+    number: 6,
+   
+  }
+]
