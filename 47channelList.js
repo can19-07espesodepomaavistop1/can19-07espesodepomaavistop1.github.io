b8b0f313fd72093032b4c618fe694a9b79fc3ecb
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-09-07 14:52:33 -0300

    Update 47channelList.js

--- 47channelList.js
+++ 47channelList.js
@@ -3,7 +3,7 @@
   name: 'can ameeeri espoo argee ',
     getURL: 'QW1lcmljYV9TcG9ydHM=',
     keyId:"a0aa01337a2148bda1e21862295ae037",
-    key:"f86f8457438d4e449d53b956cc1be664",
+    key:"fee0f87a0527497351fc54ce36ad1de2",
     number: 6,
    
   }
