3ebbd6da37f9aa7eac21ad63e5492d3c09ec978c
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-09-09 21:05:38 -0300

    Create 52channelList.js

---
+++ 52channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'can geen pyy ',
+    getURL: 'R0VOX1RW',
+    keyId: '49eb924ba998ca7fbbaee30dcef1ecb4',
+    key: '6e131b04b2e87598fb588ac205673698',
+    number: 7,
+   
+  }
+]
