2a1caea2effb74ced9bac847c96da730a62b2d33
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-09-17 03:42:55 -0300

    Create 58channelList.js

---
+++ 58channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'can paaamooo ',
+    getURL: 'UGFyYW1vdW50',
+    keyId:"b85b710ecff3e38f31fc8e249b1c1cef",
+    key: "a1544c193dde6f8858c9358ee69a60a7",
+    number: 7,
+   
+  }
+]
