2288b775bf2b1bcbe023219d653006d575a099ca
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-09-20 02:04:51 -0300

    Create 73channelList.js

---
+++ 73channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'can hbb444',
+    getURL: 'SEJPX0ZhbWlseQ==',
+    keyId: "535473132f68082160b489482ca35f8e",
+    key:  "e90fd91fcd3fc809bed2b1c0d37f7297",
+    number: 6,
+   
+  }
+]
