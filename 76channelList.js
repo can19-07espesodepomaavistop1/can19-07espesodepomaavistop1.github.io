d062213562919c18d784ee358b99c33d5d817ee2
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-09-20 02:13:00 -0300

    Create 76channelList.js

---
+++ 76channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'can hbb7 poo',
+    getURL: 'SEJPX1BPUA==',
+    keyId: "f4e1ce5cef7e9a110fe968f8881b21fa",
+    key:  "6bbe2062b150b11496cdd5fbdd9c89d6",
+    number: 7,
+   
+  }
+]
