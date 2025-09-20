82d4779a77be53ae7affae80427a226e67cd5e6e
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-09-20 01:44:56 -0300

    Create 65channelList.js

---
+++ 65channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'can aaaeqqqii',
+    getURL: 'QVhOSEQ=',
+    keyId:"03e77b5bbf5135a85d587c5af2f2f730",
+    key: "1dd1ab1938a791d03f09eb88478b4880",
+    number: 7,
+   
+  }
+]
