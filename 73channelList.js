2d7680272757a58dcd7c19168c503386dff25e70
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-09-20 02:08:34 -0300

    Update 73channelList.js

--- 73channelList.js
+++ 73channelList.js
@@ -1,6 +1,6 @@
  const channelList = [
   {
-  name: 'can hbb444',
+  name: 'can hbb444 faami',
     getURL: 'SEJPX0ZhbWlseQ==',
     keyId: "535473132f68082160b489482ca35f8e",
     key:  "e90fd91fcd3fc809bed2b1c0d37f7297",
