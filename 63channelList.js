332280617b1e809d6be4291727430cf8b5c753f9
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-09-20 01:40:05 -0300

    Create 63channelList.js

---
+++ 63channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'can teeteenoovee ',
+    getURL: 'VEJT',
+    keyId:"49a0179b034ae899cb67d8a5834181aa",
+    key:"486e2c6d69adea7e17f2960e8e366612",
+    number: 6,
+   
+  }
+]
