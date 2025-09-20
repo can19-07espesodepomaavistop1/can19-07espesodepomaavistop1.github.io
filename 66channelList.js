3d94d1bc66694362c06ad613c8643a419fe414d8
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-09-20 01:46:59 -0300

    Create 66channelList.js

---
+++ 66channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'can eeefeqqqii',
+    getURL: 'RlhIRA==',
+    keyId:"9ab9c42c713e0fb5517dc77cc19d6755",
+    key:  "d8c271b82387e609b6c19e5e79240aff",
+    number: 3,
+   
+  }
+]
