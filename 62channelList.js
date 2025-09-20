457327c9313e6152e63a303e07ee39a44be06c80
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-09-20 01:36:15 -0300

    Update 62channelList.js

--- 62channelList.js
+++ 62channelList.js
@@ -1,6 +1,6 @@
  const channelList = [
   {
-  name: 'can teeteeseer ',
+  name: 'can teeteeseeer ',
     getURL: 'VE5UU2VyaWVz',
     keyId:"5c14d6aeb19bf113edf91a7b544253cb",
     key: "8d776d6f9c5c840ebe88d767dd731692",
