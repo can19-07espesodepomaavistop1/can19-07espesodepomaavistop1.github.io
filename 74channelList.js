8919d480d06dd81f3893b0c253a2a7dd73109860
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-09-20 02:08:52 -0300

    Create 74channelList.js

---
+++ 74channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'can hbb55muudii',
+    getURL: 'SEJPX011bmRp',
+    keyId: "7821b2662148fe333d5191acbb8a5c1f",
+    key:  "ae43f3e2545237ebda58b13813b5d328",
+    number: 6,
+   
+  }
+]
