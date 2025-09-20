6d3037239b6c2c8d52dfb17d5db018e964017408
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-09-20 01:51:22 -0300

    Create 68channelList.js

---
+++ 68channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'can deeehee',
+    getURL: 'REhF',
+    keyId:"ff800156f81f70c687eccaadedc2ca15",
+    key: "937ba1301e27008b34d02b9d8a31a0db",
+    number: 7,
+   
+  }
+]
