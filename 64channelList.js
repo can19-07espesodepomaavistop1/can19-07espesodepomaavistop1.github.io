6ed10c140a96134f01eabbe277111abb6e3d1b2a
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-09-20 01:42:11 -0300

    Create 64channelList.js

---
+++ 64channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'can aaameesee ',
+    getURL: 'QU1D',
+    keyId:"1fa6a86fd80b6d1425dde74482b5dc9c",
+    key:"744c3636d57a651ab444cf9fe5f95b33",
+    number: 3,
+   
+  }
+]
