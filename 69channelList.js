ddedc39f7dc02dda45c2ab49c56a980659fae674
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-09-20 01:53:22 -0300

    Create 69channelList.js

---
+++ 69channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'can usssaaneeeo',
+    getURL: 'VVNBX05ldHdvcms=',
+    keyId:"c754b522b3b87dc2ba2da3e35154ec14",
+    key: "141a51e9e83861ae99b4b217431710b3",
+    number: 7,
+   
+  }
+]
