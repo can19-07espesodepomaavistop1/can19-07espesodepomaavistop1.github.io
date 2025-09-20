a5dfca05d8975062edbc8a0d36e69faafe7bcc23
can19-07espesodepomaavistop1 <candepomasvistosop3@protonmail.com>
2025-09-20 02:15:10 -0300

    Create 77channelList.js

---
+++ 77channelList.js
@@ -0,0 +1,10 @@
+ const channelList = [
+  {
+  name: 'can hbb8 estree',
+    getURL: 'SEJPX0V4dHJlbWU=',
+    keyId: "d504011bbef467c0de3a7534e302a6d4",
+    key: "334103e2abe16faa7f9ab2e3097c5a58",
+    number: 6,
+   
+  }
+]
